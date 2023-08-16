ab83da9a338ab867efd4910cd0fd7d128cd8367d
mateuszborowskiep <45963093+mateuszborowskiep@users.noreply.github.com>
2023-08-16 10:54:24 +0200

    changes in variables

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,3 +1,12 @@
+variable "project_id" {
+    type = string
+    default = "rosy-crawler-389806"
+}
+variable "region_eu" {
+    type = string
+    default = "europe-west1"
+}
+
 resource "google_storage_bucket" "raw" {
   project = var.project_id
   name = "${var.project_id}-raw"
