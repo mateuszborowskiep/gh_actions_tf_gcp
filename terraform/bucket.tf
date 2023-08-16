1a4a146b56df261b0346cd07c6681d9ec4308b37
mateuszborowskiep <45963093+mateuszborowskiep@users.noreply.github.com>
2023-08-16 10:47:31 +0200

    changes in variables

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -4,5 +4,4 @@ resource "google_storage_bucket" "raw" {
   force_destroy = false
   uniform_bucket_level_access = true
   location = var.region
-  labels = local.labels
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
 variable "project" {
     type= string
     description = "ID Google project"
+    default = "rosy-crawler-389806"
 }
 
 variable "region" {
     type= string
     description = "Region Google project"
-}
-
-variable  "data-project" {
-    type = string
-    description = "Name of data pipeline project to use as resource prefix"
+    default = "europe-west1"
 }
